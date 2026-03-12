2bd9e23c6d94636c925e84b00978476436ac81f3
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-12 18:59:13 +0000

    feat: add sorting helper

---
+++ services/gamma_620.rb
@@ -0,0 +1,19 @@
+module HashExtensions
+  VERSION = "2.7.7"
+
+  def self.serialize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts HashExtensions::VERSION
+puts HashExtensions.calculate(32, 50, :*)
+puts HashExtensions.clamp(114, 0, 100)
