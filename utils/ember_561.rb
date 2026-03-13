681f14df1dc909194367f157beb1df24e68e8aa8
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-13 19:05:01 +0000

    chore: misc improvements

---
+++ utils/ember_561.rb
@@ -0,0 +1,18 @@
+numbers = (10..20).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(3).to_a
+total   = numbers.sum
+
+puts "Range   : 10..20"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"cedar" => 60, "yankee" => 38, "delta" => 48, "gamma" => 60}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
