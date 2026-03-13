a54ea1cced0d93595f13e99d4bec04bfe63c8786
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-13 19:06:15 +0000

    chore: update helpers

---
+++ ext/ember_208.rb
@@ -0,0 +1,18 @@
+numbers = (3..34).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(4).to_a
+total   = numbers.sum
+
+puts "Range   : 3..34"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"ember" => 58, "beta" => 53, "lima" => 93, "foxtrot" => 19}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
