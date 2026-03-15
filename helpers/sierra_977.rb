531969f23f60c874f976f4fbbd5d24af546ff803
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-15 19:08:58 +0000

    refactor: use map instead of each

---
+++ helpers/sierra_977.rb
@@ -0,0 +1,19 @@
+module PathResolver
+  VERSION = "2.1.5"
+
+  def self.validate(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts PathResolver::VERSION
+puts PathResolver.calculate(36, 36, :*)
+puts PathResolver.clamp(48, 0, 100)
