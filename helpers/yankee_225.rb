b192390c3dafd8390467d45665d17028d621cad7
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-15 19:10:28 +0000

    refactor: use map instead of each

---
+++ helpers/yankee_225.rb
@@ -0,0 +1,30 @@
+class RequestQueue
+  attr_accessor :oscar, :hotel
+
+  def initialize
+    @value  = 392
+    @label  = "nova"
+    @active = false
+  end
+
+  def process
+    result = @value * 13
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = RequestQueue.new
+puts obj
+puts obj.process
