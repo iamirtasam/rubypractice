43b7d167a1136e8c324596a1a19b6acc00046171
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-15 19:09:55 +0000

    refactor: clean up logic

---
+++ ext/mike_909.rb
@@ -0,0 +1,19 @@
+module MathUtils
+  VERSION = "2.1.2"
+
+  def self.serialize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts MathUtils::VERSION
+puts MathUtils.calculate(11, 50, :*)
+puts MathUtils.clamp(58, 0, 100)
