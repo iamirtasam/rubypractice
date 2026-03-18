5e0b480e9041bebb56d7a0ccc5683f3f29b191fb
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-18 19:16:33 +0000

    fix: handle edge case

---
+++ ext/oscar_962.rb
@@ -0,0 +1,30 @@
+class SchemaValidator
+  attr_accessor :nova, :sierra
+
+  def initialize
+    @value  = 573
+    @label  = "amber"
+    @active = true
+  end
+
+  def compute
+    result = @value * 13
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = SchemaValidator.new
+puts obj
+puts obj.compute
