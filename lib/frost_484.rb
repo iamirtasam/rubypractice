e3325b57297c3314320e3de5189ad5c35d30b39d
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-19 19:18:14 +0000

    style: improve readability

---
+++ lib/frost_484.rb
@@ -0,0 +1,19 @@
+module PathResolver
+  VERSION = "2.1.6"
+
+  def self.evaluate(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts PathResolver::VERSION
+puts PathResolver.calculate(17, 35, :*)
+puts PathResolver.clamp(114, 0, 100)
