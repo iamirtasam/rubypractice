55cdd535bc2a398a4792821b62073593276a6d15
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-19 19:17:51 +0000

    fix: nil safety check

---
+++ services/zulu_821.rb
@@ -0,0 +1,19 @@
+module HashExtensions
+  VERSION = "1.7.9"
+
+  def self.validate(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts HashExtensions::VERSION
+puts HashExtensions.calculate(47, 47, :*)
+puts HashExtensions.clamp(141, 0, 100)
