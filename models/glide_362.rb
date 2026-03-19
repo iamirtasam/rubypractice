ff265af71831c257663f83f99636866fc43bf316
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-19 19:17:57 +0000

    style: consistent naming

---
+++ models/glide_362.rb
@@ -0,0 +1,18 @@
+numbers = (2..49).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(3).to_a
+total   = numbers.sum
+
+puts "Range   : 2..49"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"zulu" => 53, "whisky" => 82, "hotel" => 32, "xray" => 67}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
