ceb590bbaf959436ec3d9cb1257b10937b0be638
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-20 19:21:26 +0000

    feat: new data structure

---
+++ core/hotel_201.rb
@@ -0,0 +1,19 @@
+module CryptoUtils
+  VERSION = "3.6.6"
+
+  def self.compute(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts CryptoUtils::VERSION
+puts CryptoUtils.calculate(10, 23, :*)
+puts CryptoUtils.clamp(153, 0, 100)
