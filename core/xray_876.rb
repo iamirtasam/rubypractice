41193db87b670d958462825f01cbacde46c549ed
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-20 19:20:37 +0000

    feat: add utility method

---
+++ core/xray_876.rb
@@ -0,0 +1,30 @@
+class RequestQueue
+  attr_accessor :mike, :oscar
+
+  def initialize
+    @value  = 999
+    @label  = "lima"
+    @active = false
+  end
+
+  def format
+    result = @value * 16
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = RequestQueue.new
+puts obj
+puts obj.format
