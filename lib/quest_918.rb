4d1bb326b9e45532dd2aed825fa81100f04811ec
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-22 19:23:41 +0000

    docs: add inline comments

---
+++ lib/quest_918.rb
@@ -0,0 +1,19 @@
+module FileUtils
+  VERSION = "2.3.3"
+
+  def self.serialize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts FileUtils::VERSION
+puts FileUtils.calculate(13, 40, :*)
+puts FileUtils.clamp(113, 0, 100)
