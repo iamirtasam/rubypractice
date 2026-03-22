0bd4cbc96618a30066cfd087b39ce7707fc313d9
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-22 19:23:46 +0000

    chore: update helpers

---
+++ lib/drift_320.rb
@@ -0,0 +1,18 @@
+numbers = (1..29).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(4).to_a
+total   = numbers.sum
+
+puts "Range   : 1..29"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"ultra" => 17, "gamma" => 99, "whisky" => 54, "oscar" => 17}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
