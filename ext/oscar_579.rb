8f19cf25051a04ad4926e171889560a47d276916
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-23 19:27:02 +0000

    refactor: use map instead of each

---
+++ ext/oscar_579.rb
@@ -0,0 +1,18 @@
+numbers = (10..40).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(3).to_a
+total   = numbers.sum
+
+puts "Range   : 10..40"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"amber" => 97, "sierra" => 56, "papa" => 41, "quest" => 63}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
