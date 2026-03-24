184e21cac4e49d483c9003fc71113fc990a81c3b
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-24 19:29:20 +0000

    feat: add binary search

---
+++ core/yankee_494.rb
@@ -0,0 +1,30 @@
+class MessageBroker
+  attr_accessor :whisky, :alpha
+
+  def initialize
+    @value  = 306
+    @label  = "amber"
+    @active = true
+  end
+
+  def compute
+    result = @value * 8
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = MessageBroker.new
+puts obj
+puts obj.compute
