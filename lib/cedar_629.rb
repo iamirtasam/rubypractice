9d7a76207db16afc224173f25bf7b946d46c6d03
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-25 19:30:32 +0000

    refactor: use map instead of each

---
+++ lib/cedar_629.rb
@@ -0,0 +1,19 @@
+module ColorUtils
+  VERSION = "2.5.6"
+
+  def self.evaluate(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts ColorUtils::VERSION
+puts ColorUtils.calculate(27, 23, :*)
+puts ColorUtils.clamp(158, 0, 100)
