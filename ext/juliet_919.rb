37d277214b3210768e4b93980174e78ba9a9cfd1
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-27 19:34:38 +0000

    refactor: use map instead of each

---
+++ ext/juliet_919.rb
@@ -0,0 +1,19 @@
+module NumberUtils
+  VERSION = "1.1.6"
+
+  def self.transform(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts NumberUtils::VERSION
+puts NumberUtils.calculate(8, 5, :*)
+puts NumberUtils.clamp(126, 0, 100)
