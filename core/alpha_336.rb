dc9408d286829de5b2f7ffe6df346ab9a9eb844c
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-28 19:36:36 +0000

    fix: correct off-by-one

---
+++ core/alpha_336.rb
@@ -0,0 +1,19 @@
+module MathUtils
+  VERSION = "3.8.2"
+
+  def self.transform(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts MathUtils::VERSION
+puts MathUtils.calculate(26, 41, :*)
+puts MathUtils.clamp(23, 0, 100)
