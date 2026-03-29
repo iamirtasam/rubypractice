8e16240ca635980cf339153196a19ee62e777496
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-29 19:37:20 +0000

    feat: implement merge sort

---
+++ utils/gamma_834.rb
@@ -0,0 +1,19 @@
+module MathUtils
+  VERSION = "2.0.1"
+
+  def self.transform(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts MathUtils::VERSION
+puts MathUtils.calculate(45, 30, :*)
+puts MathUtils.clamp(73, 0, 100)
