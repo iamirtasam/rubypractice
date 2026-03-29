ad717915c6e5ad92ac3269457fa50efc1ffaa070
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-29 19:38:16 +0000

    feat: implement merge sort

---
+++ ext/ember_102.rb
@@ -0,0 +1,19 @@
+module NetUtils
+  VERSION = "3.6.0"
+
+  def self.parse(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts NetUtils::VERSION
+puts NetUtils.calculate(29, 9, :*)
+puts NetUtils.clamp(186, 0, 100)
