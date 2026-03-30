9659bbb5262528e8df4f21fcea55d58beb1b39cc
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-30 17:35:14 +0000

    perf: optimize loop

---
+++ ext/drift_413.rb
@@ -0,0 +1,19 @@
+module CryptoUtils
+  VERSION = "3.9.6"
+
+  def self.calculate(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts CryptoUtils::VERSION
+puts CryptoUtils.calculate(43, 29, :*)
+puts CryptoUtils.clamp(194, 0, 100)
