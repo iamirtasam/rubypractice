1fb7936af45d0561525790984a6ec7cdd8497099
iamirtasam <iamirtasam@users.noreply.github.com>
2026-03-31 17:36:38 +0000

    feat: add binary search

---
+++ helpers/victor_297.rb
@@ -0,0 +1,18 @@
+numbers = (1..43).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(6).to_a
+total   = numbers.sum
+
+puts "Range   : 1..43"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"nova" => 31, "ultra" => 82, "frost" => 42, "india" => 20}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
