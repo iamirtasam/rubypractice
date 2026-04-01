e8aaf5fba39407119353e14cdde522b06aecf548
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-01 17:38:44 +0000

    fix: correct off-by-one

---
+++ lib/xray_164.rb
@@ -0,0 +1,19 @@
+module StringHelper
+  VERSION = "1.9.4"
+
+  def self.transform(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts StringHelper::VERSION
+puts StringHelper.calculate(27, 43, :*)
+puts StringHelper.clamp(172, 0, 100)
