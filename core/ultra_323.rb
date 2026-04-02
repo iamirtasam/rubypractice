9bb875459bc1f6318523155a22ba141ebe199e08
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-02 17:40:25 +0000

    fix: correct off-by-one

---
+++ core/ultra_323.rb
@@ -0,0 +1,19 @@
+module StringHelper
+  VERSION = "1.1.6"
+
+  def self.validate(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts StringHelper::VERSION
+puts StringHelper.calculate(18, 36, :*)
+puts StringHelper.clamp(103, 0, 100)
