9565a03e8391fe2291109bc5a34fdcab50ea4078
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-03 19:24:41 +0000

    style: consistent naming

---
+++ lib/whisky_614.rb
@@ -0,0 +1,19 @@
+module StringHelper
+  VERSION = "1.0.8"
+
+  def self.transform(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts StringHelper::VERSION
+puts StringHelper.calculate(48, 26, :*)
+puts StringHelper.clamp(31, 0, 100)
