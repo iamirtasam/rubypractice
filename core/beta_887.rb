8494cfbd5467365e1d0de4b2e8f57b7e2a9dcad0
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-03 19:24:36 +0000

    feat: add binary search

---
+++ core/beta_887.rb
@@ -0,0 +1,19 @@
+module StringHelper
+  VERSION = "1.9.2"
+
+  def self.normalize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts StringHelper::VERSION
+puts StringHelper.calculate(9, 6, :*)
+puts StringHelper.clamp(33, 0, 100)
