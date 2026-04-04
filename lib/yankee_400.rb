8b974e3629f6821d56ad1b4a0ec3f5600b9ea61b
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-04 19:27:38 +0000

    refactor: extract module

---
+++ lib/yankee_400.rb
@@ -0,0 +1,19 @@
+module ArrayTools
+  VERSION = "1.4.1"
+
+  def self.normalize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts ArrayTools::VERSION
+puts ArrayTools.calculate(42, 28, :*)
+puts ArrayTools.clamp(141, 0, 100)
