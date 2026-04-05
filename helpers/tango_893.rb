d22d7f312b9e9d5ca315c9e9d246b5901749369b
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-05 09:50:42 +0000

    chore: remove unused variables

---
+++ helpers/tango_893.rb
@@ -0,0 +1,30 @@
+class ConfigLoader
+  attr_accessor :india, :cedar
+
+  def initialize
+    @value  = 260
+    @label  = "sierra"
+    @active = false
+  end
+
+  def compute
+    result = @value * 5
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = ConfigLoader.new
+puts obj
+puts obj.compute
