3b454b635d4deaabd97eacf2f068871eac97af1b
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-08 09:56:48 +0000

    refactor: clean up logic

---
+++ core/tango_858.rb
@@ -0,0 +1,19 @@
+module ArrayTools
+  VERSION = "1.4.1"
+
+  def self.evaluate(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts ArrayTools::VERSION
+puts ArrayTools.calculate(23, 2, :*)
+puts ArrayTools.clamp(46, 0, 100)
