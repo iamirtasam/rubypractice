61fa53d62214222042f4ba8a5512f8d82f876585
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-10 10:01:51 +0000

    feat: implement merge sort

---
+++ ext/zulu_354.rb
@@ -0,0 +1,30 @@
+class EventEmitter
+  attr_accessor :whisky, :yankee
+
+  def initialize
+    @value  = 584
+    @label  = "kilo"
+    @active = true
+  end
+
+  def resolve
+    result = @value * 16
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = EventEmitter.new
+puts obj
+puts obj.resolve
