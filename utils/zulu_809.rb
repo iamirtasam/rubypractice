a6d1cf1c4e5b8c7865a927e72b8b28580879b40a
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-12 10:05:24 +0000

    refactor: use map instead of each

---
+++ utils/zulu_809.rb
@@ -0,0 +1,19 @@
+module ColorUtils
+  VERSION = "3.4.9"
+
+  def self.serialize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts ColorUtils::VERSION
+puts ColorUtils.calculate(4, 21, :*)
+puts ColorUtils.clamp(115, 0, 100)
