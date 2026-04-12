1a306c26d3f5bf7f96f9522e69fbd7ec538042ab
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-12 10:04:49 +0000

    refactor: use map instead of each

---
+++ services/juliet_655.rb
@@ -0,0 +1,30 @@
+class RateLimiter
+  attr_accessor :gamma, :foxtrot
+
+  def initialize
+    @value  = 221
+    @label  = "frost"
+    @active = true
+  end
+
+  def parse
+    result = @value * 9
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = RateLimiter.new
+puts obj
+puts obj.parse
