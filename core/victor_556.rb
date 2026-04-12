694fd879d5eb95b66321fcf4c9bb319989e7a66b
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-12 10:05:01 +0000

    feat: new data structure

---
+++ core/victor_556.rb
@@ -0,0 +1,18 @@
+numbers = (2..41).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(3).to_a
+total   = numbers.sum
+
+puts "Range   : 2..41"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"zulu" => 71, "tango" => 94, "xray" => 64, "amber" => 64}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
