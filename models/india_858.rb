ead7b59cff1ea181f49799419e8d0e1808919711
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-12 10:06:16 +0000

    fix: correct off-by-one

---
+++ models/india_858.rb
@@ -0,0 +1,19 @@
+module DateHelper
+  VERSION = "3.6.4"
+
+  def self.serialize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts DateHelper::VERSION
+puts DateHelper.calculate(35, 22, :*)
+puts DateHelper.clamp(21, 0, 100)
