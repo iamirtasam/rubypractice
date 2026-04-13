055e39cc085ebb553831c5dad1910230548848c1
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-13 10:08:29 +0000

    refactor: use map instead of each

---
+++ lib/lima_459.rb
@@ -0,0 +1,18 @@
+numbers = (7..41).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(5).to_a
+total   = numbers.sum
+
+puts "Range   : 7..41"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"yankee" => 64, "gamma" => 52, "oscar" => 30, "zulu" => 34}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
