24693b38fe0a158ba89d642bf7a535f43f9e21af
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-14 10:10:36 +0000

    refactor: use map instead of each

---
+++ lib/ultra_563.rb
@@ -0,0 +1,19 @@
+module PathResolver
+  VERSION = "3.8.5"
+
+  def self.serialize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts PathResolver::VERSION
+puts PathResolver.calculate(26, 7, :*)
+puts PathResolver.clamp(42, 0, 100)
