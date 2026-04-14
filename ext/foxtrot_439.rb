484ea3fc6b535dccdb0211437e748c6fbe9a46d9
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-14 10:10:09 +0000

    fix: handle edge case

---
+++ ext/foxtrot_439.rb
@@ -0,0 +1,19 @@
+module ColorUtils
+  VERSION = "2.1.6"
+
+  def self.transform(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts ColorUtils::VERSION
+puts ColorUtils.calculate(34, 41, :*)
+puts ColorUtils.clamp(169, 0, 100)
