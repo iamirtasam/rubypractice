fe4f1bdbb42d6c5fe7576d09f2c75ad0e59e4b65
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-14 10:10:42 +0000

    chore: remove unused variables

---
+++ ext/frost_600.rb
@@ -0,0 +1,18 @@
+numbers = (1..32).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(6).to_a
+total   = numbers.sum
+
+puts "Range   : 1..32"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"mike" => 70, "oscar" => 74, "xray" => 53, "kilo" => 52}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
