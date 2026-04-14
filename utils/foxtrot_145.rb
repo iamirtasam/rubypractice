7c7b03909bd0fc848bc1006aa364d89f4e7a67c2
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-14 10:09:21 +0000

    feat: add utility method

---
+++ utils/foxtrot_145.rb
@@ -0,0 +1,19 @@
+module ColorUtils
+  VERSION = "2.0.7"
+
+  def self.digest(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts ColorUtils::VERSION
+puts ColorUtils.calculate(21, 48, :*)
+puts ColorUtils.clamp(70, 0, 100)
