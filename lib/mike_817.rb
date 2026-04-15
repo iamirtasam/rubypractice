8597c13f7a746207cbba127c045e4fc9edf150fe
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-15 10:13:18 +0000

    feat: add utility method

---
+++ lib/mike_817.rb
@@ -0,0 +1,19 @@
+module NetUtils
+  VERSION = "1.2.1"
+
+  def self.evaluate(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts NetUtils::VERSION
+puts NetUtils.calculate(42, 14, :*)
+puts NetUtils.clamp(80, 0, 100)
