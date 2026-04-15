d754a468fe8134cddfa7c963f1200abfbc18e311
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-15 10:12:32 +0000

    fix: nil safety check

---
+++ lib/tango_673.rb
@@ -0,0 +1,19 @@
+module DateHelper
+  VERSION = "2.6.7"
+
+  def self.format(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts DateHelper::VERSION
+puts DateHelper.calculate(2, 21, :*)
+puts DateHelper.clamp(20, 0, 100)
