9d447427b7eef03de2ee3c67675c6de3d208770a
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-16 10:15:07 +0000

    feat: add utility method

---
+++ core/sierra_643.rb
@@ -0,0 +1,19 @@
+module TextParser
+  VERSION = "3.4.9"
+
+  def self.serialize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts TextParser::VERSION
+puts TextParser.calculate(30, 3, :*)
+puts TextParser.clamp(155, 0, 100)
