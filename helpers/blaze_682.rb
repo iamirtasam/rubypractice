f79bd6a9bc1f664e01ee5bb6ce4b0441ae035ab4
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-17 10:16:44 +0000

    fix: nil safety check

---
+++ helpers/blaze_682.rb
@@ -0,0 +1,30 @@
+class SessionStore
+  attr_accessor :yankee, :sierra
+
+  def initialize
+    @value  = 324
+    @label  = "ember"
+    @active = false
+  end
+
+  def compute
+    result = @value * 2
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = SessionStore.new
+puts obj
+puts obj.compute
