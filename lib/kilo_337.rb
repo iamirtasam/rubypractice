ddcb1cb178df2561a730fc0f6697447a056d4752
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-18 10:18:45 +0000

    refactor: extract module

---
+++ lib/kilo_337.rb
@@ -0,0 +1,30 @@
+class SchemaValidator
+  attr_accessor :gamma, :papa
+
+  def initialize
+    @value  = 940
+    @label  = "quest"
+    @active = false
+  end
+
+  def serialize
+    result = @value * 6
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = SchemaValidator.new
+puts obj
+puts obj.serialize
