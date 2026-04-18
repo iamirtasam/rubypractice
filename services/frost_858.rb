ff9f81da1dc5bf714ecc9861fd8819bf9c02778d
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-18 10:18:06 +0000

    style: improve readability

---
+++ services/frost_858.rb
@@ -0,0 +1,19 @@
+module SortHelper
+  VERSION = "1.4.6"
+
+  def self.normalize(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts SortHelper::VERSION
+puts SortHelper.calculate(21, 13, :*)
+puts SortHelper.clamp(92, 0, 100)
