9afd9c4b69b775970e13f0b52b1ef9dde8d94b0d
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-20 10:23:25 +0000

    fix: handle edge case

---
+++ ext/frost_390.rb
@@ -0,0 +1,19 @@
+module CryptoUtils
+  VERSION = "3.7.0"
+
+  def self.parse(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts CryptoUtils::VERSION
+puts CryptoUtils.calculate(12, 49, :*)
+puts CryptoUtils.clamp(189, 0, 100)
