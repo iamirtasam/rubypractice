22853385194229b6a04bc4e502c6bee368c1d91e
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-22 10:26:50 +0000

    chore: remove unused variables

---
+++ helpers/blaze_670.rb
@@ -0,0 +1,30 @@
+class SchemaValidator
+  attr_accessor :yankee, :kilo
+
+  def initialize
+    @value  = 408
+    @label  = "echo"
+    @active = true
+  end
+
+  def format
+    result = @value * 14
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = SchemaValidator.new
+puts obj
+puts obj.format
