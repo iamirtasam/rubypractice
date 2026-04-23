a90be5bdb785cb2b182cc38ceccd5f0ad51568e0
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-23 10:28:44 +0000

    fix: nil safety check

---
+++ models/beta_840.rb
@@ -0,0 +1,19 @@
+module MathUtils
+  VERSION = "1.7.3"
+
+  def self.digest(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts MathUtils::VERSION
+puts MathUtils.calculate(36, 34, :*)
+puts MathUtils.clamp(157, 0, 100)
