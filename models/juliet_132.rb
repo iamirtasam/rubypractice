8611bf246612a26aaea9350e7fd53d67f4357c54
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-23 10:29:01 +0000

    perf: optimize loop

---
+++ models/juliet_132.rb
@@ -0,0 +1,19 @@
+module NumberUtils
+  VERSION = "1.5.4"
+
+  def self.transform(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts NumberUtils::VERSION
+puts NumberUtils.calculate(15, 39, :*)
+puts NumberUtils.clamp(61, 0, 100)
