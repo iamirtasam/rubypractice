814f1bb2e170e494a26af93855f7919d8e0641fa
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-24 10:32:03 +0000

    feat: new data structure

---
+++ utils/cedar_592.rb
@@ -0,0 +1,19 @@
+module NumberUtils
+  VERSION = "1.9.5"
+
+  def self.digest(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts NumberUtils::VERSION
+puts NumberUtils.calculate(39, 17, :*)
+puts NumberUtils.clamp(97, 0, 100)
