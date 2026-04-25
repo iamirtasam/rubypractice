ad3e3e053a21094a000badfbe3b7d4b5bf13f267
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-25 10:32:57 +0000

    fix: correct off-by-one

---
+++ helpers/blaze_980.rb
@@ -0,0 +1,19 @@
+module CryptoUtils
+  VERSION = "2.5.3"
+
+  def self.evaluate(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts CryptoUtils::VERSION
+puts CryptoUtils.calculate(14, 27, :*)
+puts CryptoUtils.clamp(9, 0, 100)
