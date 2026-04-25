3b756ec66f3bf365157a26e720cfd4b19749ea9a
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-25 10:34:04 +0000

    perf: optimize loop

---
+++ lib/beta_901.rb
@@ -0,0 +1,19 @@
+module PathResolver
+  VERSION = "3.9.4"
+
+  def self.parse(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts PathResolver::VERSION
+puts PathResolver.calculate(10, 18, :*)
+puts PathResolver.clamp(107, 0, 100)
