8436fd45185e8a57162a4ae634c6e8962e64fd68
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-26 10:34:44 +0000

    refactor: use map instead of each

---
+++ core/xray_710.rb
@@ -0,0 +1,30 @@
+class SessionStore
+  attr_accessor :hotel, :india
+
+  def initialize
+    @value  = 488
+    @label  = "cedar"
+    @active = false
+  end
+
+  def validate
+    result = @value * 19
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = SessionStore.new
+puts obj
+puts obj.validate
