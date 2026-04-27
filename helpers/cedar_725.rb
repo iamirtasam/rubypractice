2823acfd6c3d6f8fc7c40c63732d0676734f77d2
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-27 10:38:03 +0000

    chore: misc improvements

---
+++ helpers/cedar_725.rb
@@ -0,0 +1,18 @@
+numbers = (1..22).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(3).to_a
+total   = numbers.sum
+
+puts "Range   : 1..22"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"tango" => 69, "beta" => 74, "oscar" => 77, "india" => 19}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
