bf2d63c383dafe0cb6cce6c038edf8b0dc3e650f
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-28 10:40:33 +0000

    style: improve readability

---
+++ ext/echo_942.rb
@@ -0,0 +1,19 @@
+module NumberUtils
+  VERSION = "3.1.0"
+
+  def self.process(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts NumberUtils::VERSION
+puts NumberUtils.calculate(4, 29, :*)
+puts NumberUtils.clamp(37, 0, 100)
