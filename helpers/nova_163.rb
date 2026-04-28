0d90e02ff0f57aa66de3853eeba4eba98355c6db
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-28 10:40:21 +0000

    chore: misc improvements

---
+++ helpers/nova_163.rb
@@ -0,0 +1,19 @@
+module MathUtils
+  VERSION = "3.3.7"
+
+  def self.format(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts MathUtils::VERSION
+puts MathUtils.calculate(13, 32, :*)
+puts MathUtils.clamp(64, 0, 100)
