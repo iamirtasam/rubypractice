61815af908de2bda5e544e49016fab4e620e2b20
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-28 10:39:41 +0000

    test: add sample output

---
+++ ext/xray_466.rb
@@ -0,0 +1,19 @@
+module PathResolver
+  VERSION = "2.2.3"
+
+  def self.format(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts PathResolver::VERSION
+puts PathResolver.calculate(40, 22, :*)
+puts PathResolver.clamp(124, 0, 100)
