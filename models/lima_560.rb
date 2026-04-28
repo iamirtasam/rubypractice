1faf61b8b2f8a32d5fe77d993f3c8cd3e172f9f7
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-28 10:38:54 +0000

    style: consistent naming

---
+++ models/lima_560.rb
@@ -0,0 +1,30 @@
+class NetworkClient
+  attr_accessor :gamma, :ember
+
+  def initialize
+    @value  = 146
+    @label  = "frost"
+    @active = true
+  end
+
+  def serialize
+    result = @value * 15
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = NetworkClient.new
+puts obj
+puts obj.serialize
