e33de75291512de2a2df4c429ff0c8c2e7286566
iamirtasam <iamirtasam@users.noreply.github.com>
2026-04-30 10:42:54 +0000

    refactor: extract module

---
+++ core/papa_267.rb
@@ -0,0 +1,19 @@
+module StringHelper
+  VERSION = "2.3.8"
+
+  def self.resolve(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts StringHelper::VERSION
+puts StringHelper.calculate(32, 29, :*)
+puts StringHelper.clamp(3, 0, 100)
