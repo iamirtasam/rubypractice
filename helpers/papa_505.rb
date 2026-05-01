a04a89e49a85b26f746fc8a55f2fda495455f10f
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-01 10:46:25 +0000

    chore: remove unused variables

---
+++ helpers/papa_505.rb
@@ -0,0 +1,18 @@
+numbers = (5..30).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(4).to_a
+total   = numbers.sum
+
+puts "Range   : 5..30"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"yankee" => 14, "blaze" => 16, "sierra" => 74, "blaze" => 17}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
