c267b3b6b8ce4217e585f3b5167c69266a4249c9
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-02 10:47:02 +0000

    style: improve readability

---
+++ utils/blaze_675.rb
@@ -0,0 +1,18 @@
+numbers = (2..31).to_a
+
+evens   = numbers.select(&:even?)
+odds    = numbers.reject(&:even?)
+squared = numbers.map { |n| n ** 2 }
+chunked = numbers.each_slice(5).to_a
+total   = numbers.sum
+
+puts "Range   : 2..31"
+puts "Evens   : \#{evens.first(5).inspect}"
+puts "Odds    : \#{odds.first(5).inspect}"
+puts "Squared : \#{squared.first(5).inspect}"
+puts "Chunks  : \#{chunked.length} groups"
+puts "Total   : \#{total}"
+
+scores = {"beta" => 96, "alpha" => 21, "blaze" => 42, "nova" => 15}
+top = scores.max_by { |_, v| v }
+puts "Top scorer: \#{top[0]} with \#{top[1]}"
