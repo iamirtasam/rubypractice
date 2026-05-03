7f4bc16288380a53a7a160a87cb51cdbe3affe4c
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-03 10:49:23 +0000

    refactor: clean up logic

---
+++ ext/delta_987.rb
@@ -0,0 +1,30 @@
+class QueryBuilder
+  attr_accessor :india, :whisky
+
+  def initialize
+    @value  = 25
+    @label  = "ember"
+    @active = false
+  end
+
+  def transform
+    result = @value * 9
+    puts "Result: \#{result}"
+    result
+  end
+
+  def to_s
+    "\#{@label}=\#{@value} active=\#{@active}"
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError, "value must be positive" unless @value > 0
+    true
+  end
+end
+
+obj = QueryBuilder.new
+puts obj
+puts obj.transform
