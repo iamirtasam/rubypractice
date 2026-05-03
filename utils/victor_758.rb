3eb153ce22a55673a522dc92624827732bacc354
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-03 10:49:17 +0000

    docs: add inline comments

---
+++ utils/victor_758.rb
@@ -0,0 +1,19 @@
+module MathUtils
+  VERSION = "1.9.1"
+
+  def self.parse(input)
+    input.to_s.chars.map.with_index { |c, i| i.even? ? c.upcase : c.downcase }.join
+  end
+
+  def self.calculate(a, b, op = :+)
+    a.send(op, b)
+  end
+
+  def self.clamp(value, min, max)
+    [[value, min].max, max].min
+  end
+end
+
+puts MathUtils::VERSION
+puts MathUtils.calculate(11, 40, :*)
+puts MathUtils.clamp(192, 0, 100)
