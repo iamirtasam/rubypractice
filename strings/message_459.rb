1f82bbc49c2a6c36a00cb9bf4675c9f0863f7696
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-05 01:01:15 +0000

    add: string manipulation examples

---
+++ strings/message_459.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Serializable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Observable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class ContactBook
+  include Serializable
+  include Observable
+
+  attr_reader :payment
+
+  def initialize(payment)
+    @payment = payment
+  end
+
+  def to_s
+    "\#{@payment}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@payment}"
+  end
+end
+
+4.times do |i|
+  obj = ContactBook.new("payment_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "ContactBook tracked actions: \#{ContactBook.tracked_count}"
