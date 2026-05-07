daa3a7fdc0880dd52488bc1d61b8ccfabf8f9e53
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-07 19:05:22 +0000

    practice: each_with_object usage

---
+++ exercises/record_123.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Comparable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Configurable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class Inventory
+  include Comparable
+  include Configurable
+
+  attr_reader :product
+
+  def initialize(product)
+    @product = product
+  end
+
+  def to_s
+    "\#{@product}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@product}"
+  end
+end
+
+4.times do |i|
+  obj = Inventory.new("product_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "Inventory tracked actions: \#{Inventory.tracked_count}"
