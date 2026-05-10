73a62e119b1918320d3ecd8be5065c3208c392b9
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-10 19:05:45 +0000

    add: proc and lambda examples

---
+++ strings/record_803.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Comparable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Observable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class Employee
+  include Comparable
+  include Observable
+
+  attr_reader :account
+
+  def initialize(account)
+    @account = account
+  end
+
+  def to_s
+    "\#{@account}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@account}"
+  end
+end
+
+4.times do |i|
+  obj = Employee.new("account_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "Employee tracked actions: \#{Employee.tracked_count}"
