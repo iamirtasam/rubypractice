cfc42288f0fcc1cb7b235cc3f17a375769c1e59c
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-11 19:12:16 +0000

    add: sorting algorithm practice

---
+++ exercises/category_456.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Printable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Configurable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class Calculator
+  include Printable
+  include Configurable
+
+  attr_reader :invoice
+
+  def initialize(invoice)
+    @invoice = invoice
+  end
+
+  def to_s
+    "\#{@invoice}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@invoice}"
+  end
+end
+
+4.times do |i|
+  obj = Calculator.new("invoice_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "Calculator tracked actions: \#{Calculator.tracked_count}"
