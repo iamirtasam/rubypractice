c178da7512cd1ea3d5c2e4ade583c2f2b9d7361f
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-16 19:05:33 +0000

    exercise: respond_to? and duck typing

---
+++ strings/report_815.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Greetable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Notifiable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class HotelReservation
+  include Greetable
+  include Notifiable
+
+  attr_reader :task
+
+  def initialize(task)
+    @task = task
+  end
+
+  def to_s
+    "\#{@task}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@task}"
+  end
+end
+
+4.times do |i|
+  obj = HotelReservation.new("task_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "HotelReservation tracked actions: \#{HotelReservation.tracked_count}"
