2641deff781eb104b8b0027e7758b62a69df1610
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-19 19:10:10 +0000

    add: freeze and immutability notes

---
+++ oop/invoice_305.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Printable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Trackable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class ShoppingCart
+  include Printable
+  include Trackable
+
+  attr_reader :post
+
+  def initialize(post)
+    @post = post
+  end
+
+  def to_s
+    "\#{@post}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@post}"
+  end
+end
+
+3.times do |i|
+  obj = ShoppingCart.new("post_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "ShoppingCart tracked actions: \#{ShoppingCart.tracked_count}"
