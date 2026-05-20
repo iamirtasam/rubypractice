ad8a38fe15e32ba8576a7416351be78f9490398c
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-20 19:07:37 +0000

    practice: string formatting methods

---
+++ exercises/item_154.rb
@@ -0,0 +1,43 @@
+# Practice: Class with Comparable and Enumerable
+
+class Student
+  include Comparable
+
+  attr_reader :post, :offset
+
+  def initialize(post, offset)
+    @post = post
+    @offset = offset.to_f
+  end
+
+  def <=>(other)
+    @offset <=> other.offset
+  end
+
+  def to_s
+    "\#{@post} (offset: \#{@offset})"
+  end
+
+  def discounted(percent)
+    discounted_val = @offset * (1 - percent / 100.0)
+    self.class.new("\#{@post}_sale", discounted_val.round(2))
+  end
+end
+
+items = [
+  Student.new("post_a", 4),
+  Student.new("post_b", 41),
+  Student.new("post_c", 64),
+  Student.new("post_d", 2),
+]
+
+puts "All items:"
+items.each { |i| puts "  \#{i}" }
+puts "Sorted    : \#{items.sort.map(&:to_s).inspect}"
+puts "Min       : \#{items.min}"
+puts "Max       : \#{items.max}"
+puts "Avg offset: \#{(items.sum(&:offset) / items.size).round(2)}"
+
+sale_items = items.map { |i| i.discounted(6) }
+puts "After \#{}% off:"
+sale_items.each { |i| puts "  \#{i}" }
