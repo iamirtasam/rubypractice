faaa9e255a89f0c8aebb8860256e52451f9bc243
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-23 19:12:42 +0000

    practice: string formatting methods

---
+++ exercises/item_314.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Printable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Cacheable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class ParkingLot
+  include Printable
+  include Cacheable
+
+  attr_reader :product
+
+  def initialize(product)
+    @product = product
+  end
+
+  def to_s
+    "\#{@product}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@product}"
+  end
+end
+
+2.times do |i|
+  obj = ParkingLot.new("product_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "ParkingLot tracked actions: \#{ParkingLot.tracked_count}"
