52f785826ce2935052a21ed80d4201c75b1d8b52
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-24 19:11:44 +0000

    practice: open classes and monkey patch

---
+++ practice/message_599.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Printable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Observable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class VendingMachine
+  include Printable
+  include Observable
+
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def to_s
+    "\#{@item}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@item}"
+  end
+end
+
+4.times do |i|
+  obj = VendingMachine.new("item_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "VendingMachine tracked actions: \#{VendingMachine.tracked_count}"
