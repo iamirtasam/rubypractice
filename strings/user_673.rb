a256d1e222cb35da4102714865fd15d96e22769a
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-27 19:05:43 +0000

    add: string manipulation examples

--- strings/user_673.rb
+++ strings/user_673.rb
@@ -1,62 +1,43 @@
-# Practice: Modules and Mixins
+# Practice: Class with Comparable and Enumerable
 
-module Loggable
-  def greet
-    "Hello, I am \#{self.class.name}: \#{to_s}"
+class VendingMachine
+  include Comparable
+
+  attr_reader :comment, :offset
+
+  def initialize(comment, offset)
+    @comment = comment
+    @offset = offset.to_f
   end
 
-  def farewell
-    "Goodbye from \#{self.class.name}"
-  end
-end
-
-module Observable
-  def self.included(base)
-    base.instance_variable_set(:@tracked_count, 0)
-    base.extend(ClassMethods)
-  end
-
-  module ClassMethods
-    def track!
-      @tracked_count = (@tracked_count || 0) + 1
-    end
-
-    def tracked_count
-      @tracked_count || 0
-    end
-  end
-
-  def log_action(action)
-    self.class.track!
-    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
-  end
-end
-
-class WeatherReport
-  include Loggable
-  include Observable
-
-  attr_reader :invoice
-
-  def initialize(invoice)
-    @invoice = invoice
+  def <=>(other)
+    @offset <=> other.offset
   end
 
   def to_s
-    "\#{@invoice}"
+    "\#{@comment} (offset: \#{@offset})"
   end
 
-  def perform
-    log_action(:perform)
-    "performed by \#{@invoice}"
+  def discounted(percent)
+    discounted_val = @offset * (1 - percent / 100.0)
+    self.class.new("\#{@comment}_sale", discounted_val.round(2))
   end
 end
 
-3.times do |i|
-  obj = WeatherReport.new("invoice_\#{i + 1}")
-  puts obj.greet
-  puts obj.perform
-  puts obj.farewell
-  puts "---"
-end
-puts "WeatherReport tracked actions: \#{WeatherReport.tracked_count}"
+items = [
+  VendingMachine.new("comment_a", 44),
+  VendingMachine.new("comment_b", 57),
+  VendingMachine.new("comment_c", 109),
+  VendingMachine.new("comment_d", 19),
+]
+
+puts "All items:"
+items.each { |i| puts "  \#{i}" }
+puts "Sorted    : \#{items.sort.map(&:to_s).inspect}"
+puts "Min       : \#{items.min}"
+puts "Max       : \#{items.max}"
+puts "Avg offset: \#{(items.sum(&:offset) / items.size).round(2)}"
+
+sale_items = items.map { |i| i.discounted(30) }
+puts "After \#{}% off:"
+sale_items.each { |i| puts "  \#{i}" }
