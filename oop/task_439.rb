5e84ecb4d7354c6a404a0bd32b696f0b4b43b67b
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-27 19:08:34 +0000

    exercise: binary search implementation

---
+++ oop/task_439.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Greetable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Trackable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class TextEditor
+  include Greetable
+  include Trackable
+
+  attr_reader :message
+
+  def initialize(message)
+    @message = message
+  end
+
+  def to_s
+    "\#{@message}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@message}"
+  end
+end
+
+3.times do |i|
+  obj = TextEditor.new("message_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "TextEditor tracked actions: \#{TextEditor.tracked_count}"
