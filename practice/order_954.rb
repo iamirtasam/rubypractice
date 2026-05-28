2c54d294367635f849a5fecbfd8877c4a6d3da94
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-28 19:06:10 +0000

    practice: mixin module design

---
+++ practice/order_954.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Printable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Observable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class ATM
+  include Printable
+  include Observable
+
+  attr_reader :comment
+
+  def initialize(comment)
+    @comment = comment
+  end
+
+  def to_s
+    "\#{@comment}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@comment}"
+  end
+end
+
+2.times do |i|
+  obj = ATM.new("comment_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "ATM tracked actions: \#{ATM.tracked_count}"
