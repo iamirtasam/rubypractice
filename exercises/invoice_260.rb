0da11ad4b25c924aa0acb9cdecdf73a6d7101011
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-28 19:05:26 +0000

    exercise: hash transform_values

---
+++ exercises/invoice_260.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Loggable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Configurable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class Student
+  include Loggable
+  include Configurable
+
+  attr_reader :entry
+
+  def initialize(entry)
+    @entry = entry
+  end
+
+  def to_s
+    "\#{@entry}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@entry}"
+  end
+end
+
+2.times do |i|
+  obj = Student.new("entry_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "Student tracked actions: \#{Student.tracked_count}"
