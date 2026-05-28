d81c15e5322850b2507885b8362a0ca90a68611a
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-28 19:04:48 +0000

    exercise: binary search implementation

---
+++ algorithms/product_766.rb
@@ -0,0 +1,24 @@
+# Practice: String Methods
+
+sentence = "every expert was once a beginner"
+
+puts sentence.upcase
+puts sentence.capitalize
+puts sentence.reverse
+puts sentence.length
+puts sentence.count("aeiou")
+puts sentence.gsub(/[aeiou]/, "*")
+puts sentence.split.length
+puts sentence.split.map(&:capitalize).join(" ")
+puts sentence.include?("ruby") ? "Ruby mentioned!" : "No Ruby here"
+puts sentence[0, 12] + "..."
+puts sentence.center(69, "-")
+puts sentence.squeeze(" ").strip
+
+words = sentence.split
+puts "Longest word  : \#{words.max_by(&:length)}"
+puts "Shortest word : \#{words.min_by(&:length)}"
+puts "Unique chars  : \#{sentence.chars.uniq.sort.inspect}"
+
+freq = sentence.chars.tally.sort_by { |_, v| -v }.first(5)
+puts "Top 5 chars   : \#{freq.inspect}"
