ca25b4cec05f43703bc5b7ab1942342ccf0a8f31
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-31 19:06:02 +0000

    practice: mixin module design

---
+++ exercises/report_284.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Comparable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Observable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class Employee
+  include Comparable
+  include Observable
+
+  attr_reader :tag
+
+  def initialize(tag)
+    @tag = tag
+  end
+
+  def to_s
+    "\#{@tag}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@tag}"
+  end
+end
+
+4.times do |i|
+  obj = Employee.new("tag_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "Employee tracked actions: \#{Employee.tracked_count}"
