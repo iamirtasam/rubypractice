d63effaef9f12b0b636b3c1897c25d233c889edf
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-31 19:06:53 +0000

    exercise: respond_to? and duck typing

---
+++ practice/tag_383.rb
@@ -0,0 +1,44 @@
+# Practice: Exception Handling and Custom Errors
+
+class TextEditorError < StandardError
+  def initialize(msg = "invalid item speed")
+    super
+  end
+end
+
+class TextEditor
+  MIN_SPEED = 9
+  MAX_SPEED = 140
+
+  def initialize(item)
+    @item = item
+    @speed = 0
+  end
+
+  def set_speed(val)
+    raise ArgumentError, "speed must be a number" unless val.is_a?(Numeric)
+    raise TextEditorError, "speed \#{val} out of [9,140] range" unless (9..140).include?(val)
+    @speed = val
+  end
+
+  def speed
+    raise TextEditorError, "speed not set" if @speed.zero?
+    @speed
+  end
+end
+
+test_values = [14, -5, 165, 76]
+
+obj = TextEditor.new("item_test")
+test_values.each do |val|
+  begin
+    obj.set_speed(val)
+    puts "Set speed = \#{val} => OK (stored: \#{obj.speed})"
+  rescue TextEditorError => e
+    puts "[TextEditorError] \#{e.message}"
+  rescue ArgumentError => e
+    puts "[ArgumentError] \#{e.message}"
+  ensure
+    puts "  -> attempted value: \#{val}"
+  end
+end
