84e9a6aa871619452c37604f8f9f67cb8d8529c5
iamirtasam <iamirtasam@users.noreply.github.com>
2026-05-31 19:06:44 +0000

    exercise: hash transform_values

---
+++ modules/payment_452.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Serializable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Configurable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class ParkingLot
+  include Serializable
+  include Configurable
+
+  attr_reader :payment
+
+  def initialize(payment)
+    @payment = payment
+  end
+
+  def to_s
+    "\#{@payment}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@payment}"
+  end
+end
+
+2.times do |i|
+  obj = ParkingLot.new("payment_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "ParkingLot tracked actions: \#{ParkingLot.tracked_count}"
