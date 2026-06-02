5acf697b28af0261768f411ebd036b94a68d5e5f
iamirtasam <iamirtasam@users.noreply.github.com>
2026-06-02 19:04:48 +0000

    practice: exception handling

---
+++ practice/category_209.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Serializable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Notifiable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class ParkingLot
+  include Serializable
+  include Notifiable
+
+  attr_reader :post
+
+  def initialize(post)
+    @post = post
+  end
+
+  def to_s
+    "\#{@post}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@post}"
+  end
+end
+
+3.times do |i|
+  obj = ParkingLot.new("post_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "ParkingLot tracked actions: \#{ParkingLot.tracked_count}"
