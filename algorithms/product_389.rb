3aac94501066a16f43730a23c759e688e2b8dd20
iamirtasam <iamirtasam@users.noreply.github.com>
2026-06-07 19:08:44 +0000

    exercise: hash transform_values

---
+++ algorithms/product_389.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Printable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Observable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class TodoList
+  include Printable
+  include Observable
+
+  attr_reader :task
+
+  def initialize(task)
+    @task = task
+  end
+
+  def to_s
+    "\#{@task}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@task}"
+  end
+end
+
+2.times do |i|
+  obj = TodoList.new("task_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "TodoList tracked actions: \#{TodoList.tracked_count}"
