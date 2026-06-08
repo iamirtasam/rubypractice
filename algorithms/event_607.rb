8ad0875a50d2bef0770be7a4ea4f829c7091b02f
iamirtasam <iamirtasam@users.noreply.github.com>
2026-06-08 19:07:18 +0000

    add: class with attr_accessor

---
+++ algorithms/event_607.rb
@@ -0,0 +1,62 @@
+# Practice: Modules and Mixins
+
+module Loggable
+  def greet
+    "Hello, I am \#{self.class.name}: \#{to_s}"
+  end
+
+  def farewell
+    "Goodbye from \#{self.class.name}"
+  end
+end
+
+module Cacheable
+  def self.included(base)
+    base.instance_variable_set(:@tracked_count, 0)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def track!
+      @tracked_count = (@tracked_count || 0) + 1
+    end
+
+    def tracked_count
+      @tracked_count || 0
+    end
+  end
+
+  def log_action(action)
+    self.class.track!
+    puts "[LOG] \#{self.class.name}#\#{action} called (total: \#{self.class.tracked_count})"
+  end
+end
+
+class ATM
+  include Loggable
+  include Cacheable
+
+  attr_reader :payment
+
+  def initialize(payment)
+    @payment = payment
+  end
+
+  def to_s
+    "\#{@payment}"
+  end
+
+  def perform
+    log_action(:perform)
+    "performed by \#{@payment}"
+  end
+end
+
+4.times do |i|
+  obj = ATM.new("payment_\#{i + 1}")
+  puts obj.greet
+  puts obj.perform
+  puts obj.farewell
+  puts "---"
+end
+puts "ATM tracked actions: \#{ATM.tracked_count}"
