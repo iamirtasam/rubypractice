c54c0d7a441eb43b53fa8933db554047b623dea0
iamirtasam <iamirtasam@users.noreply.github.com>
2026-06-09 19:05:31 +0000

    practice: string formatting methods

---
+++ collections/payment_187.rb
@@ -0,0 +1,44 @@
+# Practice: Exception Handling and Custom Errors
+
+class TextEditorError < StandardError
+  def initialize(msg = "invalid message height")
+    super
+  end
+end
+
+class TextEditor
+  MIN_HEIGHT = 3
+  MAX_HEIGHT = 154
+
+  def initialize(message)
+    @message = message
+    @height = 0
+  end
+
+  def set_height(val)
+    raise ArgumentError, "height must be a number" unless val.is_a?(Numeric)
+    raise TextEditorError, "height \#{val} out of [3,154] range" unless (3..154).include?(val)
+    @height = val
+  end
+
+  def height
+    raise TextEditorError, "height not set" if @height.zero?
+    @height
+  end
+end
+
+test_values = [114, -3, 179, 111]
+
+obj = TextEditor.new("message_test")
+test_values.each do |val|
+  begin
+    obj.set_height(val)
+    puts "Set height = \#{val} => OK (stored: \#{obj.height})"
+  rescue TextEditorError => e
+    puts "[TextEditorError] \#{e.message}"
+  rescue ArgumentError => e
+    puts "[ArgumentError] \#{e.message}"
+  ensure
+    puts "  -> attempted value: \#{val}"
+  end
+end
