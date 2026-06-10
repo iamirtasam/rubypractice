e714092bffbe75df58a27a44755f06fdaae2678c
iamirtasam <iamirtasam@users.noreply.github.com>
2026-06-10 19:05:24 +0000

    practice: Comparable module

---
+++ collections/message_273.rb
@@ -0,0 +1,43 @@
+# Practice: Class with Comparable and Enumerable
+
+class HotelReservation
+  include Comparable
+
+  attr_reader :invoice, :discount
+
+  def initialize(invoice, discount)
+    @invoice = invoice
+    @discount = discount.to_f
+  end
+
+  def <=>(other)
+    @discount <=> other.discount
+  end
+
+  def to_s
+    "\#{@invoice} (discount: \#{@discount})"
+  end
+
+  def discounted(percent)
+    discounted_val = @discount * (1 - percent / 100.0)
+    self.class.new("\#{@invoice}_sale", discounted_val.round(2))
+  end
+end
+
+items = [
+  HotelReservation.new("invoice_a", 15),
+  HotelReservation.new("invoice_b", 30),
+  HotelReservation.new("invoice_c", 98),
+  HotelReservation.new("invoice_d", 10),
+]
+
+puts "All items:"
+items.each { |i| puts "  \#{i}" }
+puts "Sorted    : \#{items.sort.map(&:to_s).inspect}"
+puts "Min       : \#{items.min}"
+puts "Max       : \#{items.max}"
+puts "Avg discount: \#{(items.sum(&:discount) / items.size).round(2)}"
+
+sale_items = items.map { |i| i.discounted(26) }
+puts "After \#{}% off:"
+sale_items.each { |i| puts "  \#{i}" }
